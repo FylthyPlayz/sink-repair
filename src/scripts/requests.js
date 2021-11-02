d015ff5d32fffd70ccb5686d7c3e69970a4cf932
FylthyPlayz <jacob_cook_football@yahoo.com>
2021-11-02 14:13:39 -0500

    Final Sink Repair

---
+++ src/scripts/requests.js
@@ -0,0 +1,32 @@
+import { getRequests } from "./dataAccess.js"
+import { deleteRequest } from "./dataAccess.js"
+
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
+
+const showRequests = (request) => {
+    return `<li> ${request.description}
+<button class="request__delete"
+id="request--${request.id}">
+Delete
+</button>
+</li>`
+
+}
+
+export const Requests = () => {
+    const requests = getRequests()
+
+    let html = `<ul>
+            ${requests.map(showRequests).join("")
+        }
+    </ul>`
+
+    return html
+}
